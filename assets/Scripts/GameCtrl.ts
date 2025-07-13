cbb3e74029c46c095fd6d4410bfbe060b2344964
Khuongduy308 <taphuongduy308@gmail.com>
2025-07-13 17:04:26 +0700

    done action fly

--- assets/Scripts/GameCtrl.ts
+++ assets/Scripts/GameCtrl.ts
@@ -1,5 +1,7 @@
-import { _decorator, Component, Node, CCInteger } from 'cc';
+import { _decorator, Component, Node, CCInteger, input, Input, EventKeyboard, KeyCode, director } from 'cc';
 import { Ground } from './Ground';
+import { Results } from './Results';
+import { Bird } from './Bird';
 const { ccclass, property } = _decorator;
 
 @ccclass('GameCtrl')
@@ -12,6 +14,17 @@ export class GameCtrl extends Component {
     public ground: Ground
 
     @property({
+        type: Results,
+        tooltip: 'results go here'
+    })
+    public result: Results;
+
+    @property({
+        type: Bird
+    })
+    public bird: Bird;
+
+    @property({
         type: CCInteger
     })
     public speed: number = 300
@@ -22,15 +35,50 @@ export class GameCtrl extends Component {
     public pipeSpeed: number = 200;
 
     onLoad(){
+        this.InitListener();
 
+        this.result.resetScore();
+
+        director.pause();
     }
 
-    initListener(){
+    InitListener(){
+        input.on(Input.EventType.KEY_DOWN, this.OnKeyDown, this);
 
+        this.node.on(Node.EventType.TOUCH_START, () => {
+            this.bird.fly();
+        })
+    }
+
+    OnKeyDown(event: EventKeyboard){
+        switch(event.keyCode){
+            case KeyCode.KEY_A:
+                this.gameOver();
+                break;
+            case KeyCode.KEY_P:
+                this.result.addScore();
+                break;
+            case KeyCode.KEY_Q:
+                this.resetGame();
+                this.bird.resetBird();
+        }
     }
 
     startGame(){
-        
+        this.result.hideResults();
+
+        director.resume();
+    }
+
+    gameOver(){
+        this.result.showResults();
+        director.pause();
+    }
+
+    resetGame(){
+        this.result.resetScore();
+
+        this.startGame();
     }
 
 }
---
+++ assets/Scripts/Pipes.ts
@@ -0,0 +1,14 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('Pipes')
+export class Pipes extends Component {
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+        
+    }
+}
+
